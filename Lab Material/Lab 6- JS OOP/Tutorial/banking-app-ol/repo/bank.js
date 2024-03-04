72445cb455f94200bbefb256c0d2852c843ca73d
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-03-04 15:26:33 +0300

    up

--- Lab Material/Lab 6- JS OOP/Tutorial/banking-app-ol/repo/bank.js
+++ Lab Material/Lab 6- JS OOP/Tutorial/banking-app-ol/repo/bank.js
@@ -26,3 +26,37 @@ export function addAccount(account) {
     accounts.push(account)
 }
 
+export function getAccounts() {
+    return accounts
+}
+
+export function withdraw(accountNo, amount) {
+    const index = accounts.findIndex(account => account.accountNo === accountNo)
+    if (index < 0) console.log('account not found');
+    else if (accounts[index].balance < amount) console.log('insufficient balance');
+    else accounts[index].balance -= amount
+}
+
+export function deposit(accountNo, amount) {
+    const index = accounts.findIndex(account => account.accountNo === accountNo)
+    if (index < 0) console.log('account not found');
+    else if (amount >= 0) accounts[index].balance += amount
+}
+
+export function deleteAccount(accountNo) {
+    const index = accounts.findIndex(account => account.accountNo === accountNo)
+    if (index < 0) console.log('account not found');
+    else accounts.splice(index, 1)
+}
+
+export function sumBalance() {
+    return accounts.reduce((acc, curr) => acc + curr.balance, 0)
+}
+export function averageBalance() {
+    return sumBalance() / accounts.length
+}
+
+export function convertFromUSToQAR(amount) {
+    return amount * 3.65;
+}
+
