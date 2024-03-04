83eb74fb961d0d2c14080c5a69ff1749df99883e
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-03-04 14:19:13 +0300

    Update bank.js

--- Lab Material/Lab 6- JS OOP/Tutorial/banking-app-ol/repo/bank.js
+++ Lab Material/Lab 6- JS OOP/Tutorial/banking-app-ol/repo/bank.js
@@ -1,1 +1,24 @@
 // npm init
+const accounts = [
+    {
+        accountNo: 123,
+        balance: 500,
+        type: 'saving'
+    },
+    {
+        accountNo: 234,
+        balance: 4000,
+        type: 'current'
+    },
+    {
+        accountNo: 345,
+        balance: 35000,
+        type: 'current'
+    },
+    {
+        accountNo: 456,
+        balance: 60000,
+        type: 'saving'
+    }
+]
+
