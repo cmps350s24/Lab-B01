37496c18ecae46d789131d9650b9ec0724d42394
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-03-04 15:33:42 +0300

    Update bank.spec.js

--- Lab Material/Lab 6- JS OOP/Tutorial/banking-app-ol/repo/bank.spec.js
+++ Lab Material/Lab 6- JS OOP/Tutorial/banking-app-ol/repo/bank.spec.js
@@ -1,1 +1,13 @@
 import { expect } from 'chai';
+import * as repo from './bank.js'
+
+describe('Bank Application', () => {
+    describe('Convert From US To QAR Method', () => {
+        it('expect 1 USD to be 3.65 QAR', () => {
+            expect(repo.convertFromUSToQAR(1)).to.equal(3.65)
+        })
+        it('expect 100 USD to be 365 QAR', () => {
+            expect(repo.convertFromUSToQAR(100)).to.equal(365)
+        })
+    })
+})
