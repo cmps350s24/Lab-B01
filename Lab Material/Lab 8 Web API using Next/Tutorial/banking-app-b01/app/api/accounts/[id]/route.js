e470654555275a5aa7751f794359c9fb4b21ed27
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-03-20 01:27:03 +0300

    up

--- Lab Material/Lab 8 Web API using Next/Tutorial/banking-app-b01/app/api/accounts/[id]/route.js
+++
@@ -1,8 +0,0 @@
-import AccountsRepo from "@/app/repo/accounts-repo"
-const accountsRepo = new AccountsRepo()
-
-export async function GET(request, { params }) {
-    const accountNo = params.id
-    const account = await accountsRepo.getAccount(accountNo)
-    return Response.json(account, { status: 200 })
-}
