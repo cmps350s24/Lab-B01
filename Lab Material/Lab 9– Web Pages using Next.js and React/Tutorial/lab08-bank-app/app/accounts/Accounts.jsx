c04c516f03cdc5bc06f432496305cf149f1a7a2b
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-04-01 14:52:20 +0300

    updated

--- Lab Material/Lab 9– Web Pages using Next.js and React/Tutorial/lab08-bank-app/app/accounts/Accounts.jsx
+++ Lab Material/Lab 9– Web Pages using Next.js and React/Tutorial/lab08-bank-app/app/accounts/Accounts.jsx
@@ -6,16 +6,30 @@ export default function Accounts({ initialAccounts }) {
 
     return (
         <>
+            <label for="acctType">
+                Account Type
+            </label>
+            <select id="acctType" onchange="handleLoadAccounts(this.value)"
+                className={styles.filterDropdown}>
+                <option value="All">All</option>
+                <option value="Saving">Saving</option>
+                <option value="Current">Current</option>
+            </select>
+            
             <table id="accounts" className={styles.table}>
-                <tr>
-                    <th>Account No</th>
-                    <th>Account Type</th>
-                    <th>Balance</th>
-                    <th>Action</th>
-                </tr>
-                {initialAccounts
-                    .map(account => <Account account={account}></Account>)
-                }
+                <thead>
+                    <tr>
+                        <th>Account No</th>
+                        <th>Account Type</th>
+                        <th>Balance</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {initialAccounts
+                        .map(account => <Account account={account}></Account>)
+                    }
+                </tbody>
             </table>
         </>
 
