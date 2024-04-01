00182c27af7080c43d42c450948c7c52e84be919
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-04-01 15:15:01 +0300

    up

--- Lab Material/Lab 9– Web Pages using Next.js and React/Tutorial/lab08-bank-app/app/accounts/Accounts.jsx
+++ Lab Material/Lab 9– Web Pages using Next.js and React/Tutorial/lab08-bank-app/app/accounts/Accounts.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
+'use client'
+import { React, useState } from 'react'
 import Account from '@/app/accounts/Account'
 import styles from '@/app/page.module.css'
 
+
+
 export default function Accounts({ initialAccounts }) {
+    const [accounts, setAccounts] = useState(initialAccounts)
+    async function handleLoadAccounts(accountType) {
+        const response = await fetch(`/api/accounts?type=${accountType}`)
+        setAccounts(await response.json())
+    }
 
     return (
         <>
-            <label for="acctType">
+            <label htmlFor="acctType">
                 Account Type
             </label>
-            <select id="acctType" onchange="handleLoadAccounts(this.value)"
+            <select id="acctType" onChange={e => handleLoadAccounts(e.target.value)}
                 className={styles.filterDropdown}>
                 <option value="All">All</option>
                 <option value="Saving">Saving</option>
                 <option value="Current">Current</option>
             </select>
-            
+
             <table id="accounts" className={styles.table}>
                 <thead>
                     <tr>
@@ -26,7 +34,7 @@ export default function Accounts({ initialAccounts }) {
                     </tr>
                 </thead>
                 <tbody>
-                    {initialAccounts
+                    {accounts
                         .map(account => <Account account={account}></Account>)
                     }
                 </tbody>
