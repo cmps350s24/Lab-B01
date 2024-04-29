b984300f3815e979dcf1fed7bed31623d64718ff
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-04-29 16:03:54 +0300

    up

--- Lab Material/Lab 11-12 Data Management Using Prisma/Tutorial/lab11-bank-app/app/repo/accounts-repo.js
+++ Lab Material/Lab 11-12 Data Management Using Prisma/Tutorial/lab11-bank-app/app/repo/accounts-repo.js
@@ -4,32 +4,79 @@ const prisma = new PrismaClient()
 class AccountsRepo {
 
     async addOwner(owner) {
-
+        try {
+            return prisma.owner.create({
+                data: owner
+            })
+        } catch (error) {
+            return {
+                error: error.message
+            }
+        }
     }
     async getOwners() {
-
+        try {
+            return prisma.owner.findMany()
+        } catch (error) {
+            return {
+                error: error.message
+            }
+        }
     }
     async getAccounts(acctType) {
+        try {
+            if (!acctType || acctType == "All") {
+                return prisma.account.findMany()
+            } else {
+                return prisma.account.findMany({
+                    where: {
+                        acctType
+                    }
+                })
+            }
+        } catch (error) {
+            return {
+                error: error.message
+            }
+        }
 
     }
     async addAccount(account) {
+        try {
+            return prisma.account.create({
+                data: account
+            })
+        } catch (error) {
+            return {
+                error: error.message
+            }
+        }
 
     }
 
     async updateAccount(accountNo, account) {
-
+        try {
+            return prisma.account.update({
+                data: account,
+                where: { accountNo: accountNo }
+            })
+        } catch (error) {
+            return {
+                error: error.message
+            }
+        }
     }
 
     async getAccount(accountNo) {
+
+    }
+    async deleteAccount(accountNo) {
 
     }
     async searchOwner(name) {
 
     }
 
-    async deleteAccount(accountNo) {
-
-    }
     async getTransactions(accountNo) {
 
     }
